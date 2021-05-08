50f03f9bbaa2e34da608da68b40aea0beed1cf6b
Navan Chauhan <navanchauhan@gmail.com>
2021-05-08 12:10:16 +0000

    autodock-vina: add 1.1.2 bottle.

--- Formula/autodock-vina.rb
+++ Formula/autodock-vina.rb
@@ -5,6 +5,12 @@ class AutodockVina < Formula
   sha256 "6d79b7e3766075fc57bacc396829fab1ab3d3b966b4e6b5e1c7f838fe90fed77"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/navanchauhan/homebrew-tap/releases/download/autodock-vina-1.1.2"
+    sha256 cellar: :any,                 catalina:     "d8d54542a7390d27cf892aa6f6109fc6c2db83ab0eeaa2dc4dd919ed42dc8ead"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c41cb8f8ddc50bc0cf964a0db8d094fa366cdd81a432ea6386d6c509120f5cba"
+  end
+
   depends_on "makedepend" => :build
   depends_on "navanchauhan/homebrew-tap/boost-for-vina"
 
