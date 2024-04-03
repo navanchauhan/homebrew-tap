5a7dfd9666cdc74ab465bba5a1d1394f1dd4b7f1
Navan Chauhan <navanchauhan@gmail.com>
2024-04-03 08:27:11 +0000

    chemicalite: add 2024.02.1 bottle.

--- Formula/chemicalite.rb
+++ Formula/chemicalite.rb
@@ -5,6 +5,12 @@ class Chemicalite < Formula
   sha256 "fa254bb9a9b15b8fb4befefc28ff12225fdf99ddcf3ee0a5bd749be72283c9e7"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://github.com/navanchauhan/homebrew-tap/releases/download/chemicalite-2024.02.1"
+    sha256 cellar: :any,                 ventura:      "b7efd14509f6a4444e9edaa2e993005a1de0372f690e555cd337bea0aad59ec6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5a968aba91e2041ddfa1664a74f4dc3b570c85ea51bd46f20d2d31ab900d4d60"
+  end
+
   depends_on "catch2" => [:build, :test]
   depends_on "cmake" => :build
   depends_on "sqlite" => [:build, :test]
