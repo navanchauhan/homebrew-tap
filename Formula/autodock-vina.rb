e659a731ea37d9c0f501793254bd93054b52dcc2
Navan Chauhan <navanchauhan@gmail.com>
2024-04-03 00:09:59 -0600

    update to 1.2.5

--- Formula/autodock-vina.rb
+++ Formula/autodock-vina.rb
@@ -1,8 +1,8 @@
 class AutodockVina < Formula
   desc "Open-source program for doing molecular docking"
-  homepage "http://vina.scripps.edu"
-  url "https://github.com/ccsb-scripps/AutoDock-Vina/archive/refs/tags/v1.2.3.tar.gz"
-  sha256 "22f85b2e770b6acc363429153b9551f56e0a0d88d25f747a40d2f55a263608e0"
+  homepage "https://vina.scripps.edu"
+  url "https://github.com/ccsb-scripps/AutoDock-Vina/archive/refs/tags/v1.2.5.tar.gz"
+  sha256 "38aec306bff0e47522ca8f581095ace9303ae98f6a64031495a9ff1e4b2ff712"
   license "Apache-2.0"
 
   depends_on "swig" => :build
@@ -12,8 +12,8 @@ class AutodockVina < Formula
     if OS.mac?
       cd "build/mac/release" do
         inreplace "Makefile" do |s|
-          s.gsub! "C_OPTIONS= -O3 -DNDEBUG -std=c++11", "C_OPTIONS= -O3 -DNDEBUG \
-          -std=c++14 -DBOOST_TIMER_ENABLE_DEPRECATED"
+          s.gsub! "C_OPTIONS= -O3 -DNDEBUG -std=c++11 -fvisibility=hidden", "C_OPTIONS= -O3 -DNDEBUG \
+          -std=c++14 -fvisibility=hidden -DBOOST_TIMER_ENABLE_DEPRECATED"
           s.gsub! "BASE=/usr/local", "BASE=#{HOMEBREW_PREFIX}"
         end
         system "make"
